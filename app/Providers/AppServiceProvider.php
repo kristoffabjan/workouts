613d8926351a41a50b02f39b9d66e11e555b2fa5
Kristof Fabjan <kristof.fabjan@agiledrop.com>
2026-01-16 14:37:32 +0100

    Login view fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Blade::anonymousComponentPath(resource_path('views/layouts'), 'layouts');
+
         $this->configureDefaults();
     }
 
