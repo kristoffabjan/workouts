490fe383232edb1f4a345e1c51d32cee720469de
Kristof Fabjan <kristof.fabjan@agiledrop.com>
2026-01-22 13:32:19 +0100

    Add Horizon service provider and configuration, update Docker Compose

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
 ];
